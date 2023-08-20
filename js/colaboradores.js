f0a13a8044a7c457b645d7a473e43a5d6ac6d56c
Cassíus Sampaio <138407825+CassiuSampaio@users.noreply.github.com>
2023-08-20 00:30:12 -0300

    20/08

---
+++ js/colaboradores.js
@@ -0,0 +1,15 @@
+
+import utilitarios from "/js/utilitarios.js";
+
+
+var colaboradores = ['mayara', 'leticia', 'sabrina', 'sandra', 'beatriz', 'evelyn'];
+var resultadoCapitalizado = utilitarios.captalizar(colaboradores);
+var resultadoCaixaAlta = utilitarios.caixaAlta(colaboradores);
+
+
+
+
+
+console. log(resultadoCapitalizados);
+console. log(resultadoCaixaAlta);
+
